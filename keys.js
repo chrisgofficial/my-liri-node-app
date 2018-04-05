39139ebba973cbcfc7ced2c2698d948d42a8fc89
Chris Gonzalez <cgonzal6911@gmail.com>
2018-04-05 14:51:00 -0400

    Add files via upload
    pushed app files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Xx0wIQhfiVZHxnLiAnMCbRf3k',
+  consumer_secret: 'HbguKmuz6j1jhUekch4i98wv6o2rM780fFgi1uOuLr6S0q2jQN',
+  access_token_key: '981956253873266689-sxcPHI88kroKuDXAwGPChUQdtmU9GVp',
+  access_token_secret: 'hvNshDOuegv0flWOfomGQsd4wMqNlVuwjgcQVgSUpERtP',
+};
+
+exports.spotify = {
+  id: process.env.331279323b36401490c792401518aefc,
+  secret: process.env.98f25a0790fa42dfadd846fd936961bd
+};
